fcd4fa1cb81fd3924139e80807b81e4026b4fd44
Kolia Huk <urenad53@ukr.net>
2020-02-22 16:58:19 +0200

    create challenge logic in view model

--- app/src/main/java/com/fourcore/presentation/challengeConstructor/ChallengeConstructorViewModel.kt
+++ app/src/main/java/com/fourcore/presentation/challengeConstructor/ChallengeConstructorViewModel.kt
@@ -1,6 +1,40 @@
 package com.fourcore.presentation.challengeConstructor
 
+import com.fourcore.SingleLiveEvent
+import com.fourcore.data.repository.ChallengeRepository
+import com.fourcore.domain.Challenge
+import com.fourcore.domain.User
 import com.fourcore.presentation.BaseViewModel
 
-class ChallengeConstructorViewModel: BaseViewModel() {
+class ChallengeConstructorViewModel(
+    val challengeRepository: ChallengeRepository
+): BaseViewModel() {
+    val challengeNotValidEvent = SingleLiveEvent<String>()
+    fun createChallenge(
+        presentationChallenge: PresentationChallenge,
+        currentUser: User
+    ) {
+        if(validateChallange(presentationChallenge)){
+            val challenge = Challenge(
+                currentUser,
+                presentationChallenge.name,
+                presentationChallenge.description,
+                1,
+                4,
+                1,
+                presentationChallenge.deadline
+            )
+            challengeRepository.insertChallenge(challenge)
+        } else {
+            challengeNotValidEvent.value = ""
+        }
+    }
+
+    private fun validateChallange(presentationChallenge: PresentationChallenge): Boolean {
+        if(presentationChallenge.name.isNullOrBlank()) {
+            return false
+        }
+        return true
+    }
+
 }
