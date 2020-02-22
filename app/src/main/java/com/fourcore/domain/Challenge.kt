74fceba1ed5ebc15889d6ab3dfe510a965072fe3
Kolia Huk <urenad53@ukr.net>
2020-02-22 16:39:01 +0200

    add award for creating challenge

--- app/src/main/java/com/fourcore/domain/Challenge.kt
+++ app/src/main/java/com/fourcore/domain/Challenge.kt
@@ -5,10 +5,10 @@ import java.util.*
 data class Challenge(
     val creator: User,
     val name: String,
-    val creationPrice: Int,
-    val award: Int,
-    val fine: Int,
     val description: String?,
+    val creationAward: Int,
+    val completeAward: Int,
+    val fine: Int,
     val deadline: Date?
 ) {
 }
