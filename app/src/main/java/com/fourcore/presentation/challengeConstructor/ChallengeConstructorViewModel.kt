cba849d28ccb1d7bd3f2efb2508eafc838bbccb7
Kolia Huk <urenad53@ukr.net>
2020-02-22 17:59:36 +0200

    provide serRepository

--- app/src/main/java/com/fourcore/presentation/challengeConstructor/ChallengeConstructorViewModel.kt
+++ app/src/main/java/com/fourcore/presentation/challengeConstructor/ChallengeConstructorViewModel.kt
@@ -2,24 +2,23 @@ package com.fourcore.presentation.challengeConstructor
 
 import com.fourcore.SingleLiveEvent
 import com.fourcore.data.repository.ChallengeRepository
+import com.fourcore.data.repository.UserRepository
 import com.fourcore.domain.Challenge
-import com.fourcore.domain.User
 import com.fourcore.presentation.BaseViewModel
 import java.util.*
 
 class ChallengeConstructorViewModel(
-    val challengeRepository: ChallengeRepository
+    val challengeRepository: ChallengeRepository,
+    val userRepository: UserRepository
 ): BaseViewModel() {
     val challengeNotValidEvent = SingleLiveEvent<String>()
-
     var deadline: Date? = null
     fun createChallenge(
-        presentationChallenge: PresentationChallenge,
-        currentUser: User
+        presentationChallenge: PresentationChallenge
     ) {
         if(validateChallange(presentationChallenge)){
             val challenge = Challenge(
-                currentUser,
+                userRepository.getCurrentUser(),
                 presentationChallenge.name,
                 presentationChallenge.description,
                 1,
