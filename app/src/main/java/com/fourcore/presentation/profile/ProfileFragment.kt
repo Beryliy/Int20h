660422ac3072fb2820c6f447e41284b7e1844bf6
radikushch <radikushch@gmail.com>
2020-02-22 22:11:56 +0200

    add bottom navigation tabs

---
+++ app/src/main/java/com/fourcore/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,8 @@
+package com.fourcore.presentation.profile
+
+import com.fourcore.NavFragment
+import com.fourcore.R
+
+class ProfileFragment : NavFragment() {
+    override fun layoutId(): Int = R.layout.fragment_profile
+}
