70c71c6d90141c064211e499c733b124f101093e
Kolia Huk <urenad53@ukr.net>
2020-02-23 01:33:16 +0200

    show created challenges

--- app/src/main/java/com/fourcore/presentation/createdChallengesLibrary/CratedChallengesFragment.kt
+++ app/src/main/java/com/fourcore/presentation/createdChallengesLibrary/CratedChallengesFragment.kt
@@ -6,19 +6,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fourcore.NavFragment
 
 import com.fourcore.R
 import kotlinx.android.synthetic.main.fragment_crated_challenges.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-/**
- * A simple [Fragment] subclass.
- */
 class CratedChallengesFragment : NavFragment() {
+    val viewModel: CreatedChallengesViewModel by viewModel()
+    val createdChallengesAdapter: ChallengesAdapter by lazy {
+        ChallengesAdapter()
+    }
     override fun layoutId() = R.layout.fragment_crated_challenges
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        createdChallengesRv.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = createdChallengesAdapter
+        }
+        viewModel.liveCreatedChalenges.observe(viewLifecycleOwner, Observer {
+            createdChallengesAdapter.updateCreatedChallenges(it)
+        })
+        viewModel.initChallengesData()
     }
 }
--- app/src/main/java/com/fourcore/presentation/createdChallengesLibrary/CreatedChallengesViewModel.kt
+++ app/src/main/java/com/fourcore/presentation/createdChallengesLibrary/CreatedChallengesViewModel.kt
@@ -1,7 +1,29 @@
 package com.fourcore.presentation.createdChallengesLibrary
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.fourcore.data.repository.ChallengeRepository
+import com.fourcore.data.repository.UserRepository
+import com.fourcore.global.util.getReadableDate
 import com.fourcore.presentation.BaseViewModel
+import kotlinx.coroutines.launch
 
-class CreatedChallengesViewModel: BaseViewModel() {
-
+class CreatedChallengesViewModel(
+    val userRepository: UserRepository,
+    val challengeRepository: ChallengeRepository
+): BaseViewModel() {
+    val liveCreatedChalenges = MutableLiveData<MutableList<CreatedChallenge>>()
+    fun initChallengesData() {
+        viewModelScope.launch {
+            val challenges = challengeRepository.getOwnerChallenges(userRepository.getCurrentUser().id!!)
+            val createdChallenges = mutableListOf<CreatedChallenge>()
+            challenges.forEach {
+                createdChallenges += CreatedChallenge(
+                    it.name,
+                    it.description ?: "",
+                    it.deadline.getReadableDate()
+                )
+            }
+        }
+    }
 }
