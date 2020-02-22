1e4141b2d4c558d7169acdd9a9c150bb20279d48
Kolia Huk <urenad53@ukr.net>
2020-02-22 16:47:24 +0200

    data class for sending challenge data into view model

---
+++ app/src/main/java/com/fourcore/presentation/challengeConstructor/PresentationChallenge.kt
@@ -0,0 +1,10 @@
+package com.fourcore.presentation.challengeConstructor
+
+import java.util.*
+
+data class PresentationChallenge(
+    val name: String,
+    val description: String?,
+    val deadline: Date
+) {
+}
